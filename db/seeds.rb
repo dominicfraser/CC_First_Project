2fc5428b2a375f55f60ca70b922046be7f5123d9
Dominic <mrdjfraser@gmail.com>
2017-03-30 22:53:06 +0100

    full week of days added and Day.all tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,32 @@ require('pry-byebug')
 monday = Day.new({
   'day_name' => 'Monday'
   })
+tuesday = Day.new({
+  'day_name' => 'Tuesday'
+  })
+wednesday = Day.new({
+  'day_name' => 'Wednesday'
+  })
+thursday = Day.new({
+  'day_name' => 'Thursday'
+  })
+friday = Day.new({
+  'day_name' => 'Friday'
+  })
+saturday = Day.new({
+  'day_name' => 'Saturday'
+  })
+sunday = Day.new({
+  'day_name' => 'Sunday'
+  })
 
 monday.save
+tuesday.save
+wednesday.save
+thursday.save
+friday.save
+saturday.save
+sunday.save
 
 binding.pry
 nil
