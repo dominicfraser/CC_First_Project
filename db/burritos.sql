7c4c247c842b48e17a3455e8167f2916d057c5df
Dominic <mrdjfraser@gmail.com>
2017-03-30 22:32:03 +0100

    initial table set up created

---
+++ db/burritos.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS menu_items;
+DROP TABLE IF EXISTS days;
+DROP TABLE IF EXISTS eateries;
+DROP TABLE IF EXISTS burgers;
+
+
+CREATE TABLE days (
+  id SERIAL2 PRIMARY KEY,
+  day_name VARCHAR(255) --check IN?
+);
+
+CREATE TABLE eateries (
+  id SERIAL4 PRIMARY KEY,
+  eatery_name VARCHAR(255)
+);
+
+CREATE TABLE burgers (
+  id SERIAL4 PRIMARY KEY,
+  burger_name VARCHAR(255)
+);
+
+CREATE TABLE menu_items (
+  id SERIAL4 PRIMARY KEY,
+  eatery_id REFERENCES eateries(id) ON DELETE CASCADE,
+  burger_id REFERENCES burgers(id) ON DELETE CASCADE,
+  price DECIMAL(19,4)
+);
+
+CREATE TABLE deals (
+  id SERIAL4 PRIMARY KEY,
+  day_id REFERENCES days(id),
+  menu_item_id REFERENCES menu_items(id),
+  deal_name VARCHAR(255)
+  -- applies_to
+  -- operator
+  -- amount
+);
