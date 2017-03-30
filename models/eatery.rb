103ae551c6f073a38a586500b8948885fef24c38
Dominic <mrdjfraser@gmail.com>
2017-03-30 22:59:04 +0100

    save method for eatery

--- models/eatery.rb
+++ models/eatery.rb
@@ -9,4 +9,12 @@ class Eatery
     @eatery_name = eatery_hash['eatery_name']
   end
 
+  ### INSTANCE METHODS
+
+  def save()
+    sql = "INSERT INTO eateries (eatery_name) VALUES ('#{eatery_name}') RETURNING id"
+    eateries_array = SqlRunner.run(sql)
+    @id = eateries_array.first['id'].to_i
+  end
+
 end
