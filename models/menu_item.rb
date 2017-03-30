237ba1b60802e722acace8eae174db61464f0d0f
Dominic <mrdjfraser@gmail.com>
2017-03-30 23:20:36 +0100

    reader and initialize for menu item

--- models/menu_item.rb
+++ models/menu_item.rb
@@ -2,10 +2,13 @@ require_relative('../db/sql_runner')
 
 class Menu_Item
 
-  attr_reader
+  attr_reader :id, :eatery_id, :burrito_id, :price
 
-  def initialize()
-
+  def initialize(mi_hash)
+    @id = mi_hash['id'].to_i
+    @eatery_id = mi_hash['eatery_id'].to_i
+    @burrito_id = mi_hash['burrito_id'].to_i
+    @price = mi_hash['price'].to_f.round(4)
   end
 
 
