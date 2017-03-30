7c9a0e06c139e9f768850a1b990463718899758b
Dominic <mrdjfraser@gmail.com>
2017-03-30 22:40:11 +0100

    initialize and reader set up

--- models/day.rb
+++ models/day.rb
@@ -2,10 +2,11 @@ require_relative('../db/sql_runner')
 
 class Day
 
-  attr_reader
+  attr_reader :id, :day_name
 
-  def initialize()
-
+  def initialize(day_hash)
+    @id = nil || day_hash['id'].to_i
+    @day_name = day_hash['day_name']
   end
 
 end
