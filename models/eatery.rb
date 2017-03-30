9ad4bbddbd9b6de2c275653e2362e5bbb7a0ed1b
Dominic <mrdjfraser@gmail.com>
2017-03-30 22:56:28 +0100

    initialize params setup and readers

--- models/eatery.rb
+++ models/eatery.rb
@@ -2,10 +2,11 @@ require_relative('../db/sql_runner')
 
 class Eatery
 
-  attr_reader
+  attr_reader :id, :eatery_name
 
-  def initialize()
-
+  def initialize(eatery_hash)
+    @id = eatery_hash['id'].to_i
+    @eatery_name = eatery_hash['eatery_name']
   end
 
 end
