cd38dbabaf217e63712238f4eedabfc7810fecf8
Dominic <mrdjfraser@gmail.com>
2017-03-30 23:09:40 +0100

    initialize and reader for Burrito

--- models/burrito.rb
+++ models/burrito.rb
@@ -2,10 +2,11 @@ require_relative('../db/sql_runner')
 
 class Burrito
 
-  attr_reader
+  attr_reader :id, :burrito_name
 
-  def initialize()
-
+  def initialize(burrito_hash)
+    @id = burrito_hash['id'].to_i
+    @burrito_name = burrito_hash['burrito_name']
   end
 
 end
