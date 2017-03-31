de8cc5fc2e9d2504a5ed8106197f7def11c1523a
Dominic <mrdjfraser@gmail.com>
2017-03-31 09:11:19 +0100

    initialize and reader for Deal

--- models/deal.rb
+++ models/deal.rb
@@ -2,10 +2,13 @@ require_relative('../db/sql_runner')
 
 class Deal
 
-  attr_reader
+  attr_reader :id, :day_id, :menu_item_id, :deal_name
 
-  def initialize()
-
+  def initialize(deal_hash)
+    @id = deal_hash['id'].to_i
+    @day_id = deal_hash['day_id'].to_i
+    @menu_item_id = deal_hash['menu_item_id'].to_i
+    @deal_name = deal_hash['deal_name']
   end
 
 end
