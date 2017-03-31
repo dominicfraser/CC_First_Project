828308ed5320eecc65571e6684b0c1b096c7d920
Dominic <mrdjfraser@gmail.com>
2017-03-31 17:39:20 +0100

    further set up of setup

--- specs/burrito_spec.rb
+++ specs/burrito_spec.rb
@@ -7,6 +7,30 @@
 class TestBurrito < MiniTest::Test
 
   def setup
+    @burrito1 = Burrito.new({
+      'burrito_name' => 'burrito6',
+      'id' => 1
+      })
+    @burrito2 = Burrito.new({
+      'burrito_name' => 'burrito6',
+      'id' => 2
+      })
+    @eatery1 = Eatery.new({
+      'eatery_name' => 'eatery1',
+      'id' => 1
+      })
+    @menu_item1 = MenuItem.new({
+      'eatery_id' => @eatery1.id,
+      'burrito_id' => @burrito1.id,
+      'price' => 9,
+      'id' => 1
+      })
+    @menu_item2 = MenuItem.new({
+      'eatery_id' => @eatery1.id,
+      'burrito_id' => @burrito2.id,
+      'price' => 9,
+      'id' => 2
+      })
 
   end
 
