0f6049babed9e0b91fe52fd06b958fb8acb87159
Dominic <mrdjfraser@gmail.com>
2017-03-31 15:37:02 +0100

    setup rewritten to use instance variables, test passing

--- specs/menu_item_spec.rb
+++ specs/menu_item_spec.rb
@@ -1,37 +1,38 @@
- require( 'minitest/autorun' )
- require_relative( '../models/menu_item' )
- require_relative( '../models/burrito' )
+ require('minitest/autorun')
+ require('minitest/rg')
+ require_relative('../models/menu_item')
+ require_relative('../models/burrito')
 
 class TestMenuItem < MiniTest::Test
 
 def setup
-  burrito1 = Burrito.new({
+  @burrito1 = Burrito.new({
     'burrito_name' => 'burrito6',
     'id' => 1
     })
-  burrito6 = Burrito.new({
+  @burrito6 = Burrito.new({
     'burrito_name' => 'burrito6',
     'id' => 6
     })
-  burrito7 = Burrito.new({
+  @burrito7 = Burrito.new({
     'burrito_name' => 'burrito7',
     'id' => 7
     })
-  burrito8 = Burrito.new({
+  @burrito8 = Burrito.new({
     'burrito_name' => 'burrito8',
     'id' => 8
     })
-  expensive_core_burritos = [burrito6,burrito7,burrito8]
-  menu_item1 = MenuItem.new({
+  @expensive_core_burritos = [@burrito6,@burrito7,@burrito8]
+  @menu_item1 = MenuItem.new({
     'eatery_id' => 1,
-    'burrito_id' => burrito7.id,
+    'burrito_id' => @burrito7.id,
     'price' => 9,
     'id' => 1
     })
 end
 
 def test_menu_item_is_in_a_selection?
-    assert_equal(false,burrito1.is_in_a_selection(expensive_core_burritos))
+    assert_equal(true,@menu_item1.is_in_a_selection?(@expensive_core_burritos))
 end
 
 end
