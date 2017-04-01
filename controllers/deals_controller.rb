bd27ef717e1ec628e70a781459974a12e688c9c5
Dominic <mrdjfraser@gmail.com>
2017-04-01 22:37:15 +0100

    burritos_applies_to working for deals/show_burritos

--- controllers/deals_controller.rb
+++ controllers/deals_controller.rb
@@ -1,9 +1,13 @@
 require_relative('../models/deal')
+require_relative('../models/burrito')
+require_relative('../models/eatery')
+require_relative('../models/day')
 
 get '/deals/:id/:eid/:did' do
 @deal = Deal.find(params[:id])
 @eatery = Eatery.find(params[:eid])
 @day = Day.find(params[:did])
+@burritos = @deal.burritos_applies_to(@eatery, @day)
 erb(:'deals/show_burritos')
 end
 
