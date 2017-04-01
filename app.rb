f3c6fbc982e2ccf796f8954d24b149a729774d2d
Dominic <mrdjfraser@gmail.com>
2017-04-01 14:34:04 +0100

    filter view tested

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require('sinatra')
 require('sinatra/contrib/all')
-
+require_relative('controllers/deals_controller')
 
 get '/' do 
-  erb(:index, :layout => :no_nav)
+  erb(:log_on, :layout => :no_nav)
 end
 
 get '' do
   redirect '/'
+end
+
+get '/deal_selection/filter' do
+  erb(:'deal_selection/filter')
 end
