c3e58d8403639815b60a4dfff399a66c2d202b77
Dominic <mrdjfraser@gmail.com>
2017-04-02 00:06:48 +0100

    deals index page createand populated

--- controllers/deals_controller.rb
+++ controllers/deals_controller.rb
@@ -4,10 +4,14 @@ require_relative('../models/eatery')
 require_relative('../models/day')
 
 get '/deals/:id/:eid/:did' do
-@deal = Deal.find(params[:id])
-@eatery = Eatery.find(params[:eid])
-@day = Day.find(params[:did])
-@burritos = @deal.burritos_applies_to(@eatery, @day)
-erb(:'deals/show_burritos')
+  @deal = Deal.find(params[:id])
+  @eatery = Eatery.find(params[:eid])
+  @day = Day.find(params[:did])
+  @burritos = @deal.burritos_applies_to(@eatery, @day)
+  erb(:'deals/show_burritos')
 end
 
+get '/deals/index' do
+  @deals = Deal.all_uniq_names
+  erb(:'deals/index')
+end
