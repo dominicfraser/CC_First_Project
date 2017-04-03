f9656a023f0010827a05be8f040e18d2b5fcf609
Dominic <mrdjfraser@gmail.com>
2017-04-03 22:36:07 +0100

    changed so that FoodCategory.all_without_first when populating the select list to then create a deal, line 96 in admin_controller

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -93,7 +93,7 @@ end
 post '/admin/deals/new' do
   @eatery = Eatery.find(params['id'])
   @days = Day.all 
-  @categories = FoodCategory.all
+  @categories = FoodCategory.all_without_first
 
   erb(:'admin/deals/new', :layout => :admin_layout)
 end
