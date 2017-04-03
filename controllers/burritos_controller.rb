189cf547325f5db632948f6dc62e77c2062a586d
Dominic <mrdjfraser@gmail.com>
2017-04-03 10:29:23 +0100

    updated directory structure and updated links that are associated with the changes

--- controllers/burritos_controller.rb
+++ controllers/burritos_controller.rb
@@ -1,7 +1,7 @@
 require_relative('../models/burrito')
 require_relative('../models/eatery')
 
-get '/burritos/index' do
+get '/admin/burritos/index' do
   @burritos = Burrito.all
-  erb(:'burritos/index', :layout => :admin_layout)
+  erb(:'admin/burritos/index', :layout => :admin_layout)
 end
