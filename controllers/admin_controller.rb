7a20917afdf00f96be4238af79207e229d2b2e17
Dominic <mrdjfraser@gmail.com>
2017-04-03 11:06:53 +0100

    admin deals index displaing basic info

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 require_relative('../models/burrito')
 require_relative('../models/eatery')
+require_relative('../models/deal')
 
 get '/admin/burritos/index' do
   @burritos = Burrito.all
@@ -9,4 +10,9 @@ end
 get '/admin/eateries/index' do
   @eateries = Eatery.all_without_first
   erb(:'admin/eateries/index', :layout => :admin_layout)
+end
+
+get '/admin/deals/index' do
+  @deals = Deal.all_uniq_names
+  erb(:'admin/deals/index', :layout => :admin_layout)
 end
